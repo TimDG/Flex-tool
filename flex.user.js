d0af5a31d8ae9738f79818733c55d78e516dc18d
TimDG <github@de-grande.be>
2023-04-14 15:56:03 +0200

    Allow users to override/change a single day's booking as well.

--- flex.user.js
+++ flex.user.js
@@ -7,10 +7,13 @@
 // @match        https://prowand.pro-unlimited.com/wand/app/worker/index.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pro-unlimited.com
 // @grant        none
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js#sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==
 // @require      https://gist.githubusercontent.com/raw/2625891/waitForKeyElements.js
+// @updateUrl    https://cdn.jsdelivr.net/gh/TimDG/Flex-tool@trunk/flex.user.js
+// @downloadUrl  https://cdn.jsdelivr.net/gh/TimDG/Flex-tool@trunk/flex.user.js
 // ==/UserScript==
 
+const usability = JSON.parse(localStorage.usability);
 
 (function() {
     'use strict';
@@ -18,19 +21,19 @@
     waitForKeyElements("#submit-time-btn", loadTimeSheets);
 })();
 
-function enterDefault(day, usability) {
-    enterHoursForDay(day, usability.defaults.hours, usability);
+function enterDefault(day) {
+    enterHoursForDay(day, usability.defaults.hours);
 }
 
-function enterHoursForDay(day, hours, usability) {
+function enterHoursForDay(day, hours) {
     const element = $(day.card);
     triggerChange(element.find("[formcontrolname='hours']")
-        .val(hours));
+                  .val(hours));
     triggerChange(element.find("[formcontrolname='regularHours']")
-        .val(hours));
+                  .val(hours));
 
     const shiftSelect = element.find("[formcontrolname='shift']");
-    fillInShift(shiftSelect, getShiftValue(day.date, usability));
+    fillInShift(shiftSelect, getShiftValue(day.date));
     triggerChange(shiftSelect);
 
     const wbs = element.find("[formcontrolname='value']:first()");
@@ -44,7 +47,7 @@ function triggerChange(field) {
     field[0].dispatchEvent(event);
 }
 
-function getShiftValue(date, usability) {
+function getShiftValue(date) {
     switch (date.getDay()) {
         case 0: return usability.defaults.sunday;
         case 6: return usability.defaults.saturday;
@@ -89,6 +92,35 @@ function loadTimeSheets() {
     $('#submit-time-btn').before(fillDefaults);
     fillDefaults.click(() => fillInDefaults(dateCards));
 
+    //Update the day buttons on the left.
+    $("#workday-sidenav li").each(function() {
+        //Fix the date.
+        var dateParts = $(this).contents()[0].textContent.trim().split("/");
+        var date = new Date(+dateParts[2], dateParts[0] - 1, +dateParts[1]);
+        $(this).contents()[0].textContent = ' ' + dateParts[1] + '/' + dateParts[0] + '/' + dateParts[2] + ' ';
+
+        //Add the edit icon.
+        const matIcon = $('<mat-icon>')
+            .text('edit')
+            .addClass('mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color')
+            .css({
+                position: 'absolute',
+                top: '50%',
+                right: '0',
+                'font-size': '15px'
+            })
+            .on('click', function() {
+                const card = dateCards.filter(card => card.date.getDate() === date.getDate())[0];
+                const hours = prompt("How many hours have you worked?");
+                if (hours != null) {
+                    enterHoursForDay(card, hours);
+                }
+            });
+        $(this)
+            .css('position', 'relative')
+            .append(matIcon);
+
+    });
 }
 
 function getDate(dateCard) {
@@ -109,9 +141,8 @@ function isHoliday(cardElement) {
 
 function fillInDefaults(cards) {
     const workDays = cards.filter(card => card.logHours);
-    const usability = JSON.parse(localStorage.usability);
 
     $.each(workDays, function(index, day) {
-        enterDefault(day, usability);
+        enterDefault(day);
     });
 }
